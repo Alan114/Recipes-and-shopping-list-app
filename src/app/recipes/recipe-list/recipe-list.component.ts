a16950a7550ced1e51e7aefac94066bbc3bf5e0d
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-22 16:29:42 +0200

    Adding a recipe to recipe list and outputting recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Smoked Salmon Crostini',
+      'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
+      'https://www.thespruceeats.com/thmb/nwp0nq_P6l7KX0jBkrFwWrH37ik=/1000x1000/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg'
+    ),
+    new Recipe(
+      'Smoked Salmon Crostini',
+      'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
+      'https://www.thespruceeats.com/thmb/nwp0nq_P6l7KX0jBkrFwWrH37ik=/1000x1000/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg'
+    ),
+  ];
 
   constructor() {}
 
