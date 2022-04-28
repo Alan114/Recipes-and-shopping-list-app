c7ce7b4438d55c68d392b8e90636b5abc39ca879
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-28 10:37:59 +0200

    Managing recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,22 +9,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Smoked Salmon Crostini',
-      'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
-      'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg'
-    ),
-    new Recipe(
-      'Strawberry Shortcake',
-      'Dazzle your tongue with strawberry shortcake made from lightly sweetened biscuits and fresh strawberries.',
-      'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg'
-    ),
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Smoked Salmon Crostini',
+      'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
+      'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg'
+    ),
+    new Recipe(
+      'Strawberry Shortcake',
+      'Dazzle your tongue with strawberry shortcake made from lightly sweetened biscuits and fresh strawberries.',
+      'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg'
+    ),
+  ];
 
-  constructor() { }
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  constructor() {}
 }
