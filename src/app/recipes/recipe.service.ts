f5d317a6660a92b1cf00cf6d1b345649e5c36d6b
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-28 13:53:04 +0200

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -11,12 +12,14 @@ export class RecipeService {
     new Recipe(
       'Smoked Salmon Crostini',
       'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
-      'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg'
+      'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg',
+      [new Ingredient('Salmon filet', 1), new Ingredient('French baguette', 1)]
     ),
     new Recipe(
       'Strawberry Shortcake',
       'Dazzle your tongue with strawberry shortcake made from lightly sweetened biscuits and fresh strawberries.',
-      'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg'
+      'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg',
+      [new Ingredient('Strawberries', 6), new Ingredient('Whipping cream', 1)]
     ),
   ];
 
