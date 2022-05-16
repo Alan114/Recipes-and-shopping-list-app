69810684f9870589e7f9ff5146f9bfe0ec9ba71f
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-16 15:49:40 +0200

    Transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,31 +10,33 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Smoked Salmon Crostini',
-      'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
-      'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg',
-      [new Ingredient('Salmon filet', 1), new Ingredient('French baguette', 1)]
-    ),
-    new Recipe(
-      'Strawberry Shortcake',
-      'Dazzle your tongue with strawberry shortcake made from lightly sweetened biscuits and fresh strawberries.',
-      'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg',
-      [new Ingredient('Strawberries', 6), new Ingredient('Whipping cream', 1)]
-    ),
-    new Recipe(
-      'Spaghetti sauce mix',
-      'Make your own delicious and easy Spaghetti Sauce Mix to store in your pantry for fresh tasting sauce...',
-      'https://www.thespruceeats.com/thmb/2Xjv9wEbjXQu2bNLUGOmd4RcwDM=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/175408128-58add6bf3df78c345bdefabc.jpg',
-      [
-        new Ingredient('Onion', 1),
-        new Ingredient('Dried parsley flakes', 1),
-        new Ingredient('Cornstarch', 1),
-        new Ingredient('Salt', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Smoked Salmon Crostini',
+  //     'Smoked salmon crostini are elegant, salty, sweet, herbaceous and fresh.',
+  //     'https://www.thespruceeats.com/thmb/xGhFPqofj4REUfz47v-pUez4LCY=/1394x784/smart/filters:no_upscale()/salmoncrostini2-5a94346deb97de00377a535c.jpg',
+  //     [new Ingredient('Salmon filet', 1), new Ingredient('French baguette', 1)]
+  //   ),
+  //   new Recipe(
+  //     'Strawberry Shortcake',
+  //     'Dazzle your tongue with strawberry shortcake made from lightly sweetened biscuits and fresh strawberries.',
+  //     'https://www.thespruceeats.com/thmb/Fu_L3kpDn2LRdHiURemlcWnMsJ4=/4000x3000/smart/filters:no_upscale()/24-5b3f8572c9e77c00378cff46.jpg',
+  //     [new Ingredient('Strawberries', 6), new Ingredient('Whipping cream', 1)]
+  //   ),
+  //   new Recipe(
+  //     'Spaghetti sauce mix',
+  //     'Make your own delicious and easy Spaghetti Sauce Mix to store in your pantry for fresh tasting sauce...',
+  //     'https://www.thespruceeats.com/thmb/2Xjv9wEbjXQu2bNLUGOmd4RcwDM=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/175408128-58add6bf3df78c345bdefabc.jpg',
+  //     [
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Dried parsley flakes', 1),
+  //       new Ingredient('Cornstarch', 1),
+  //       new Ingredient('Salt', 1),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
