3831e00e89031ecfd785b79a1bc87fc04ae0a452
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-17 10:41:17 +0200

    Preparing the signup request

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -36,7 +36,7 @@ export class RecipeService {
   //   ),
   // ];
 
-  private recipes: Recipe[];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
