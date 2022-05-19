e044f0eedae7eec17641b8e56c5582906c5ca330
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-19 12:41:23 +0200

    Understanding shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,13 @@ import { RecipesModule } from './recipes/recipes.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -33,6 +22,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     RecipesModule,
     AppRoutingModule,
     ShoppingListModule,
+    SharedModule,
   ],
   providers: [
     {
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -22,8 +22,8 @@ import { RecipesComponent } from './recipes.component';
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    CommonModule,
     RecipesRoutingModule,
+    SharedModule,
   ],
   exports: [
     RecipesComponent,
