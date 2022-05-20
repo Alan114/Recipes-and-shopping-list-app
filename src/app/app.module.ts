8d8013afe2f9cc3aac2fbb2a324905239b14a17b
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-20 14:30:53 +0200

    Setting up the NgRx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,6 +17,7 @@ import { CoreModule } from './core.module';
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
     CoreModule,
   ],
   bootstrap: [AppComponent],
