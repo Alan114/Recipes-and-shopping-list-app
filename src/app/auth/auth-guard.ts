c09f42e7c64513dc011bf717b6de659a0ac5ff75
Alan Pecenkovic <alanquad@yahoo.com>
2022-05-30 17:16:04 +0200

    Finishing up auth

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -6,19 +6,29 @@ import {
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { map, Observable, take, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, take, tap } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | Promise<boolean> | Observable<boolean | UrlTree> {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
+      map((authState) => {
+        return authState.user;
+      }),
       map((user) => {
         const isAuth = !!user;
         if (isAuth) {
